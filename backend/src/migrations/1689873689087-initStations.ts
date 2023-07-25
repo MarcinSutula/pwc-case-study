017fa3a1e796f231e57af312397efd30e7e802d1
Marcin Sutuła <59089221+MarcinSutula@users.noreply.github.com>
2023-07-25 15:39:16 +0200

    backend-switched migrations

--- backend/src/migrations/1689873689087-initStations.ts
+++ backend/src/migrations/1689873689087-initStations.ts
@@ -4,7 +4,7 @@ import shellStationsJson from "../static/shellPolishStations.json";
 import orlenStationsJson from "../static/orlenPolishStations.json";
 import { generateBrandStations } from "../helpers/station-migration";
 
-export class InitStations1689873689086 implements MigrationInterface {
+export class InitStations1689873689087 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     const bpStations = await generateBrandStations(bpStationsJson);
     const shellStations = await generateBrandStations(shellStationsJson);
